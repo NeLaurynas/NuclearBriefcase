f66b8a0d9b231b7fa2391b8e5a58d700cd3baa9a
NeLaurynas <ndisposable@gmail.com>
2024-12-17 23:00:00 +0200

    pio program move to module

--- src/modules/numbers/numbers.c
+++ src/modules/numbers/numbers.c
@@ -1,7 +1,7 @@
 // Copyright (C) 2024 Laurynas 'Deviltry' Ekekeke
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include <numbers.pio.h>
+#include <pio_numbers.pio.h>
 #include <hardware/dma.h>
 #include <hardware/pio.h>
 
@@ -34,10 +34,10 @@ void numbers_init() {
 	dma_channel_configure(MOD_NUM_DMA_CH, &dma_c, &MOD_NUM_PIO->txf[MOD_NUM_SM], buffer, 1, false);
 
 	// init PIO
-	const uint offset = pio_add_program(MOD_NUM_PIO, &numbers_program);
+	const uint offset = pio_add_program(MOD_NUM_PIO, &pio_numbers_program);
 	hard_assert(offset > 0); // TODO: led blinking error module
 	pio_sm_claim(MOD_NUM_PIO, MOD_NUM_SM);
-	numbers_program_init(MOD_NUM_PIO, MOD_NUM_SM, offset, MOD_NUM_DISP7, MOD_NUM_DISP6, MOD_NUM_DISP5, MOD_NUM_DISP4,
+	pio_numbers_program_init(MOD_NUM_PIO, MOD_NUM_SM, offset, MOD_NUM_DISP7, MOD_NUM_DISP6, MOD_NUM_DISP5, MOD_NUM_DISP4,
 	                     MOD_NUM_DISP3, MOD_NUM_DISP2, MOD_NUM_DISP1, MOD_NUM_DPGROUND2, MOD_NUM_DPGROUND1, 10000.0f);
 	pio_sm_set_enabled(MOD_NUM_PIO, MOD_NUM_SM, true);
 
